64f6d5791abb71f1a8b9752a5f539c234b9a9171
Dan Dumitriu <dandumitriu33@gmail.com>
2021-03-08 15:34:32 +0200

    Add MessageHub class w/ SendMessageToAll

---
+++ SignalRTesting/CodeOpi/Hubs/MessageHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CodeOpi.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public Task SendMessageToAll(string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
