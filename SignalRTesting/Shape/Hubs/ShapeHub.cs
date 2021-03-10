52fdcbfb69c77a0ce50514c435e00645fffd8b89
Dan Dumitriu <dandumitriu33@gmail.com>
2021-03-10 12:37:16 +0200

    Add Shape project with hub and Startup config

---
+++ SignalRTesting/Shape/Hubs/ShapeHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shape.Hubs
+{
+    public class ShapeHub : Hub
+    {
+        public async Task MoveShape(int x, int y)
+        {
+            await Clients.Others.SendAsync("shapeMoved", x, y);
+        }
+    }
+}
---
+++ SignalRTesting/Shape/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shape.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
